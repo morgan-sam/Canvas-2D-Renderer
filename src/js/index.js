0e5d0300925db20ef68de9ab92f8f1d491bf2c68
Sam <samuel_morgan_@outlook.com>
2020-01-06 14:53:59 +0000

    canvas renders matrix of specified colours

--- src/js/index.js
+++ src/js/index.js
@@ -5,22 +5,29 @@ canvas.height = 480;
 canvas.width = 640;
 let context = canvas.getContext('2d');
 
-Math.random() >= 0.5
-    ? (context.fillStyle = 'green')
-    : (context.fillStyle = 'purple');
 let x = 0;
 let y = 0;
-let width = 5;
-let height = 5;
 
-let object = [['red', 'orange', 'yellow'], ['blue', 'green', 'purple']];
-console.log(object[0][1]);
+let object = [
+    ['red', 'orange', 'yellow'],
+    ['blue', 'green', 'purple'],
+    ['grey', 'black', 'white'],
+];
 
-for (let y = 0; y < canvas.height; y += height) {
-    for (let x = 0; x < canvas.width; x += width) {
-        Math.random() >= 0.5
-            ? (context.fillStyle = 'green')
-            : (context.fillStyle = 'purple');
-        context.fillRect(x, y, width, height);
+let pixelWidth = canvas.width / object[0].length;
+let pixelHeight = canvas.height / object.length;
+
+let xPixels = object[0].length;
+let yPixels = object.length;
+
+for (let y = 0; y < yPixels; y++) {
+    for (let x = 0; x < xPixels; x++) {
+        context.fillStyle = object[y][x];
+        context.fillRect(
+            x * pixelWidth,
+            y * pixelHeight,
+            pixelWidth,
+            pixelHeight,
+        );
     }
 }
